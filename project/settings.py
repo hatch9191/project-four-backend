316238ac5a37bb11825c7294c32f67b095687569
Harry Evans <henryjohnevans@icloud.com>
2021-09-09 12:07:22 +0100

    initial commit

--- project/settings.py
+++ project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'painterest-db',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
+'this is a change'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
